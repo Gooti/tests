14eb18868594acff28484b5dd7760ec78f617958
Gooti <mateusz.urbaniak@gmail.com>
2016-11-20 22:05:05 +0100

    assert and uncomment + workspace

--- TestsOne/src/test/java/com/tests/RegistrationPage.java
+++ TestsOne/src/test/java/com/tests/RegistrationPage.java
@@ -11,67 +11,76 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertEquals;
 
 public class RegistrationPage {
 
     private WebDriver driver;
 
-    @BeforeClass // Runs this method before the first test method in the current class is invoked
+    @BeforeClass
     public void setUp() {
         // Create a new instance of the Firefox driver
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
     }
 
-    @Test (description = "1. Open page http://demoqa.com/")
+    @Test (description = "1. Fill in registration page http://demoqa.com/")
     public void registrationPage() throws InterruptedException {
 
-        String password = "ThisIsGoodPassword123!";
 
+        String password = "ThisIsGoodPassword123!";
         driver.get("http://demoqa.com/");
 //        driver.findElement(By.xpath("//div[contains(@class,'col-md-4')]//img[contains(@alt,'pattern-14')]")).click();
 //        driver.navigate().back();
         driver.navigate().refresh();
-        driver.findElement(By.xpath("//a[contains(.,'Registration')]")).click();
-        driver.findElement(By.name("first_name")).sendKeys("Mateusz");
-        driver.findElement(By.name("last_name")).sendKeys("Urbaniak");
+        String title = driver.getTitle();
+        System.out.println(title);
+        WebElement registration = driver.findElement(By.xpath("//a[contains(.,'Registration')]"));
+        registration.click();
+
+
+        WebElement firstName = driver.findElement(By.name("first_name"));
+        firstName.sendKeys("Mateusz");
+        WebElement lastName = driver.findElement(By.name("last_name"));
+        lastName.sendKeys("Urbaniak");
         WebElement element = driver.findElement(By.xpath("//div[contains(@class,'radio_wrap')]//input[contains(@value,'single')]"));
-        if (element.isSelected()){
+        if (element.isSelected()) {
             System.out.println("Already clicked.");
         } else element.click();
-        driver.findElement(By.xpath("//div[contains(@class,'radio_wrap')]//input[contains(@value,'reading')]")).click();
+        WebElement radioReading = driver.findElement(By.xpath("//div[contains(@class,'radio_wrap')]//input[contains(@value,'reading')]"));
+        radioReading.click();
         WebElement listCountry = driver.findElement(By.xpath("//select[contains(@id,'dropdown_7')]//option[contains(@value,'Andorra')]"));
         System.out.println("Value 'Country' = " + listCountry);
-        driver.findElement(By.xpath("//select[contains(@id,'mm_date_8')]//option[contains(@value,'1')]")).click();
-        driver.findElement(By.xpath("//select[contains(@id,'dd_date_8')]//option[contains(@value,'12')]")).click();
-        driver.findElement(By.xpath("//select[contains(@id,'yy_date_8')]//option[contains(@value,'2014')]")).click();
+        WebElement mmDate = driver.findElement(By.xpath("//select[contains(@id,'mm_date_8')]//option[contains(@value,'1')]"));
+        mmDate.click();
+        WebElement ddDate = driver.findElement(By.xpath("//select[contains(@id,'dd_date_8')]//option[contains(@value,'12')]"));
+        ddDate.click();
+        WebElement yyDate = driver.findElement(By.xpath("//select[contains(@id,'yy_date_8')]//option[contains(@value,'2014')]"));
+        yyDate.click();
         WebElement number = driver.findElement(By.name("phone_9"));
         number.sendKeys("ThisIsNotNumber");
-        driver.findElement(By.name("username")).sendKeys("INeedBetterUsername");
-        driver.findElement(By.name("e_mail")).sendKeys("mail@mail.com");
-        driver.findElement(By.name("description")).sendKeys("Roses are red. I have nth special to say.");
+        WebElement userName = driver.findElement(By.name("username"));
+        userName.sendKeys("INeedBetterUsername");
+        WebElement mail = driver.findElement(By.name("e_mail"));
+        mail.sendKeys("mail@mail.com");
+        WebElement description = driver.findElement(By.name("description"));
+        description.sendKeys("Roses are red. I have nth special to say.");
         driver.findElement(By.id("password_2")).sendKeys(password);
         driver.findElement(By.id("confirm_password_password_2")).sendKeys(password);
-        if (number != null){
+        if (password.equals("ThisIsGoodPassword123!")) {
             number.clear();
             number.sendKeys("0123456789");
-        } else number.sendKeys("9876543210");
-        WebElement submit = driver.findElement(By.xpath("//input[contains(@value,'Submit')]"));
-        submit.click();
-        driver.wait();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        } else number.clear();
+        number.sendKeys("9876543210");
+        assertEquals(title, "Demoqa | Just another WordPress site");
+
+    }
 
 
-
-
-        }
-
-
-//    @AfterClass // Runs this method after all the test methods in the current class have been run
-//    public void tearDown() {
-//        // Close all browser windows and safely end the session
-//        driver.quit();
-//    }
+    @AfterClass
+    public void tearDown() {
+        // Close all browser windows and safely end the session
+        driver.quit();
+    }
 
 }
--- TestsOne/src/test/java/com/tests/TestOne.java
+++ TestsOne/src/test/java/com/tests/TestOne.java
@@ -44,20 +44,6 @@ public class TestOne {
         }
 
 
-
-
-//    @Test // Marking this method as part of the test
-//    public void gotoSeleniumWikiPageFailure() {
-//        // Go to the Wikipedia home page
-//        driver.get("https://en.wikipedia.org/");
-//        // Find the text input element by its id and type "Selenium"
-//        driver.findElement(By.id("searchInput")).sendKeys("Selenium");
-//        // Click search button
-//        driver.findElement(By.id("searchButton")).click();
-//        // Get text from header of the Selenium page
-//        String header = driver.findElement(By.id("firstHeading")).getText();
-//        // Verify that header equals "Selenium WebDriver"
-//        Assert.assertEquals(header, "Selenium WebDriver");
 //    }
 
     @AfterClass // Runs this method after all the test methods in the current class have been run
